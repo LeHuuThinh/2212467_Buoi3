c7e0fa587ff18940d70f7144ea15aff3161bd722
LeHuuThinh <2212467@dlu.edu.vn>
2024-08-23 10:52:15 +0700

    Add files via upload

---
+++ BT2_1/frmBai4.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace BT2_1
+{
+    partial class frmBai4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.libDSSo = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSo = new System.Windows.Forms.TextBox();
+            this.btnTimSo = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblKetQua = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // libDSSo
+            // 
+            this.libDSSo.FormattingEnabled = true;
+            this.libDSSo.Location = new System.Drawing.Point(84, 109);
+            this.libDSSo.Name = "libDSSo";
+            this.libDSSo.Size = new System.Drawing.Size(269, 238);
+            this.libDSSo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(459, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập số cần tìm:";
+            // 
+            // txtSo
+            // 
+            this.txtSo.Location = new System.Drawing.Point(552, 106);
+            this.txtSo.Name = "txtSo";
+            this.txtSo.Size = new System.Drawing.Size(100, 20);
+            this.txtSo.TabIndex = 2;
+            // 
+            // btnTimSo
+            // 
+            this.btnTimSo.Location = new System.Drawing.Point(552, 153);
+            this.btnTimSo.Name = "btnTimSo";
+            this.btnTimSo.Size = new System.Drawing.Size(75, 23);
+            this.btnTimSo.TabIndex = 3;
+            this.btnTimSo.Text = "Tìm số";
+            this.btnTimSo.UseVisualStyleBackColor = true;
+            this.btnTimSo.Click += new System.EventHandler(this.btnTimSo_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(462, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Kết quả:";
+            // 
+            // lblKetQua
+            // 
+            this.lblKetQua.AutoSize = true;
+            this.lblKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKetQua.ForeColor = System.Drawing.Color.Red;
+            this.lblKetQua.Location = new System.Drawing.Point(548, 248);
+            this.lblKetQua.Name = "lblKetQua";
+            this.lblKetQua.Size = new System.Drawing.Size(0, 20);
+            this.lblKetQua.TabIndex = 5;
+            // 
+            // frmBai4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblKetQua);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnTimSo);
+            this.Controls.Add(this.txtSo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.libDSSo);
+            this.Name = "frmBai4";
+            this.Text = "frmBai4";
+            this.Load += new System.EventHandler(this.frmBai4_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox libDSSo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSo;
+        private System.Windows.Forms.Button btnTimSo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblKetQua;
+    }
+}
---
+++ BT2_1/frmChinh.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace BT2_1
+{
+    partial class frmChinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.lêHữuThịnhToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TsmiBai1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.TsmiBai2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.TsmiBai3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.TsmiBai4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lêHữuThịnhToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // lêHữuThịnhToolStripMenuItem
+            // 
+            this.lêHữuThịnhToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TsmiBai1,
+            this.TsmiBai2,
+            this.TsmiBai3,
+            this.TsmiBai4});
+            this.lêHữuThịnhToolStripMenuItem.Name = "lêHữuThịnhToolStripMenuItem";
+            this.lêHữuThịnhToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.lêHữuThịnhToolStripMenuItem.Text = "Lê Hữu Thịnh";
+            // 
+            // TsmiBai1
+            // 
+            this.TsmiBai1.Image = global::BT2_1.Properties.Resources.Note1;
+            this.TsmiBai1.Name = "TsmiBai1";
+            this.TsmiBai1.Size = new System.Drawing.Size(180, 22);
+            this.TsmiBai1.Text = "Bài 1";
+            this.TsmiBai1.Click += new System.EventHandler(this.TsmiBai1_Click);
+            // 
+            // TsmiBai2
+            // 
+            this.TsmiBai2.Image = global::BT2_1.Properties.Resources.Note2;
+            this.TsmiBai2.Name = "TsmiBai2";
+            this.TsmiBai2.Size = new System.Drawing.Size(180, 22);
+            this.TsmiBai2.Text = "Bài 2";
+            this.TsmiBai2.Click += new System.EventHandler(this.TsmiBai2_Click);
+            // 
+            // TsmiBai3
+            // 
+            this.TsmiBai3.Image = global::BT2_1.Properties.Resources.Note3;
+            this.TsmiBai3.Name = "TsmiBai3";
+            this.TsmiBai3.Size = new System.Drawing.Size(180, 22);
+            this.TsmiBai3.Text = "Bài 3";
+            this.TsmiBai3.Click += new System.EventHandler(this.TsmiBai3_Click);
+            // 
+            // TsmiBai4
+            // 
+            this.TsmiBai4.Image = global::BT2_1.Properties.Resources.Car2;
+            this.TsmiBai4.Name = "TsmiBai4";
+            this.TsmiBai4.Size = new System.Drawing.Size(180, 22);
+            this.TsmiBai4.Text = "Bài 4";
+            this.TsmiBai4.Click += new System.EventHandler(this.TsmiBai4_Click);
+            // 
+            // frmChinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmChinh";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem lêHữuThịnhToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem TsmiBai1;
+        private System.Windows.Forms.ToolStripMenuItem TsmiBai2;
+        private System.Windows.Forms.ToolStripMenuItem TsmiBai3;
+        private System.Windows.Forms.ToolStripMenuItem TsmiBai4;
+    }
+}
+
